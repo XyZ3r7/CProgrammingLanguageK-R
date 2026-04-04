1038fe19a3105d1ac9030d0007cb14ef27b906a3
xyz3r7 <XyZ3r7@outlook.com>
2026-04-03 22:41:53 -0400

    HelloWorld.c

---
+++ Unit1/HelloWorld.c
@@ -0,0 +1,6 @@
+/* -*- compile-command: "gcc -Wall -Werror -std=c89 test.c -o test" -*- */
+#include <stdio.h>
+
+main() {
+  printf("hello, world\n"); // printf require \n to output everything in buffer  
+}  
