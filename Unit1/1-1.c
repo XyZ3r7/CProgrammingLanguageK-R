31a21724fb2663ff4bc965fa2091904e370da4b6
xyz3r7 <XyZ3r7@outlook.com>
2026-04-03 22:50:25 -0400

    Practice 1-1, 1-2

---
+++ Unit1/1-1.c
@@ -0,0 +1,4 @@
+#include <stdio.h>  //Remove #: Unknown Type include
+main() {
+    printf("Error Tester.\n"); //Remove stdio.h: implicitly declaring library function
+}  
---
+++ Unit1/1-2.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+main() {
+  printf("Tester for adding \c \n"); // Warning: Unknown escape sequence: "\c"
+                                     // result: Tester for adding c  
+}  
