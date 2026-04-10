78e77799db101181fe9969d40cf988329766fbef
xyz3r7 <XyZ3r7@outlook.com>
2026-04-10 18:42:38 -0400

    I feel my state machine is completing.

--- Unit2/2-1.c
+++ Unit2/2-1.c
@@ -1,6 +1,13 @@
-#include "stdio.h"
-int main(){
-    printf("To C or not to C, that is the question.\n");
-    printf("To C or not to C, that is the question.\n");
+#include <stdio.h>
+#include <limits.h>
+#include <float.h>
+
+int main() {
+    printf("char: %d to %d\n", CHAR_MIN, CHAR_MAX);
+    printf("int: %d to %d\n", INT_MIN, INT_MAX);
+    printf("short: %d to %d\n", SHRT_MIN, SHRT_MAX);
+    //some day I will
+    printf("long: %ld to %ld\n", LONG_MIN, LONG_MAX);
+
     return 0;
 }
